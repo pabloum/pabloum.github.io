07f3fbea688f493df2223e783133cecfde4a6927
PABLO <pablo.um00@gmail.com>
2018-09-24 17:35:10 -0500

    Not everything is shown in the same page

--- Scripts/app.js
+++ Scripts/app.js
@@ -1,8 +1,21 @@
 $(document).ready(function() {
 
-  $(".nav-link").on("click", function() {
-    $(".active").removeClass("active");
+  $(".caja-informacion").hide();
+  $("#about-me").show();
+
+  $(document).on("click", ".nav-link", function() {
+    $("#li-about-me").removeClass("active");
+    $(".nav-link").removeClass("active");
     $(this).addClass("active");
+    $(".caja-informacion").hide();
+    $(this.hash).show();
+  });
+
+  $(document).on("click", "#home", function() {
+    $(".nav-link").removeClass("active");
+    $("#li-about-me").addClass("active");
+    $(".caja-informacion").hide();
+    $("#about-me").show();
   });
 
 });
